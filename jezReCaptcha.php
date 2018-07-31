25cb67b0e2a9e097365d674648ead262a0a75272
Alejo Sotelo <alejoasotelo@outlook.com>
2018-07-31 17:43:40 -0300

    Agregué la libreria recaptcha v3. Modifiqué el plugin para que soporte recaptcha v3.

--- jezReCaptcha.php
+++ jezReCaptcha.php
@@ -15,61 +15,70 @@
 defined('_JEXEC') or die();
 
 // require the helper class
-require_once( dirname( __FILE__ ).DS.'jezReCaptcha'.DS.'helper.php' );
+require_once(dirname(__FILE__).DS.'jezReCaptcha'.DS.'helper.php');
 
-class plgSystemJezReCaptcha extends JPlugin {
-	// variable to hold the plugin params
-	var $params = null;
+class plgSystemJezReCaptcha extends JPlugin
+{
+    // variable to hold the plugin params
+    public $params = null;
 
-	// construct plugin
-	function plgSystemJezReCaptcha(&$subject, $config) {
-		parent::__construct($subject, $config);
+    // construct plugin
+    public function plgSystemJezReCaptcha(&$subject, $config)
+    {
+        parent::__construct($subject, $config);
 
-		// get plugin params if enabled
-		$this->params = plgSystemJezReCaptchaHelper::isEnabled();
-	}
+        // get plugin params if enabled
+        $this->params = plgSystemJezReCaptchaHelper::isEnabled();
+    }
 
-	// function to execute at system event onAfterInitialise
-	function onAfterInitialise() {
-		// is JEZ reCAPTCHA Integrator plugin enabled and set to auto-verify?
-	 	if ($this->params && $this->params->def('auto_verify', 1))
-			plgSystemJezReCaptchaHelper::verifyCaptcha($this->params);
-	}
+    // function to execute at system event onAfterInitialise
+    public function onAfterInitialise()
+    {
+        // is JEZ reCAPTCHA Integrator plugin enabled and set to auto-verify?
+        if ($this->params && $this->params->def('auto_verify', 1)) {
+            plgSystemJezReCaptchaHelper::verifyCaptcha($this->params);
+        }
+    }
 
-	// function to execute at system event onAfterRender
-	function onAfterRender() {
-		global $mainframe;
-		if (!$mainframe->isAdmin()) {
-			// get the document body
-			$body = JResponse::getBody();
+    // function to execute at system event onAfterRender
+    public function onAfterRender()
+    {
+        global $mainframe;
+        if (!$mainframe->isAdmin()) {
+            // get the document body
+            $body = JResponse::getBody();
 
-			// is JEZ reCAPTCHA Integrator plugin and inclusion syntax enabled?
-		 	if ($this->params && $this->params->def('inclusion_syntax', 0) && plgSystemJezReCaptchaHelper::getDocType() == 'html') {
-				// html page, parse inclusion syntax
-				$altered = plgSystemJezReCaptchaHelper::parseInclusionSyntax($body, $this->params);
-			} elseif (!$mainframe->isAdmin()) {
-				// clear inclusion syntax
-				$altered = plgSystemJezReCaptchaHelper::clearInclusionSyntax($body);
-			}
+            // is JEZ reCAPTCHA Integrator plugin and inclusion syntax enabled?
+            if ($this->params && $this->params->def('inclusion_syntax', 0) && plgSystemJezReCaptchaHelper::getDocType() == 'html') {
+                // html page, parse inclusion syntax
+                $altered = plgSystemJezReCaptchaHelper::parseInclusionSyntax($body, $this->params);
+            } elseif (!$mainframe->isAdmin()) {
+                // clear inclusion syntax
+                $altered = plgSystemJezReCaptchaHelper::clearInclusionSyntax($body);
+            }
 
-			// set the new document body
-			if ($altered)
-				JResponse::setBody($body);
-		}
-	}
+            // set the new document body
+            if ($altered) {
+                JResponse::setBody($body);
+            }
+        }
+    }
 
-	// function to execute at custom event onCaptchaDisplay
-	function onCaptchaDisplay() {
-		// is JEZ reCAPTCHA Integrator plugin enabled and configured?
-		if ($this->params)
-			echo plgSystemJezReCaptchaHelper::renderCaptcha($this->params);
-	}
+    // function to execute at custom event onCaptchaDisplay
+    public function onCaptchaDisplay()
+    {
+        // is JEZ reCAPTCHA Integrator plugin enabled and configured?
+        if ($this->params) {
+            echo plgSystemJezReCaptchaHelper::renderCaptcha($this->params);
+        }
+    }
 
-	// function to execute at custom event onCaptchaConfirm
-	function onCaptchaConfirm() {
-		// is JEZ reCAPTCHA Integrator plugin enabled and configured?
-		if ($this->params)
-			plgSystemJezReCaptchaHelper::verifyCaptcha($this->params);
-	}
+    // function to execute at custom event onCaptchaConfirm
+    public function onCaptchaConfirm()
+    {
+        // is JEZ reCAPTCHA Integrator plugin enabled and configured?
+        if ($this->params) {
+            plgSystemJezReCaptchaHelper::verifyCaptcha($this->params);
+        }
+    }
 }
-?>
--- jezReCaptcha/helper.php
+++ jezReCaptcha/helper.php
@@ -2,11 +2,11 @@
 /*
  * JEZ reCAPTCHA Integrator Joomla! 1.5 plugin
  *
- * @package		JEZ reCAPTCHA Integrator
- * @version		1.0.0
- * @author		JoomlaEZ.com
- * @copyright	Copyright (C) 2008 JoomlaEZ.com. All rights reserved
- * @license		Creative Commons Attribution-Noncommercial-Share Alike 3.0 Unported
+ * @package        JEZ reCAPTCHA Integrator
+ * @version        1.0.0
+ * @author        JoomlaEZ.com
+ * @copyright    Copyright (C) 2008 JoomlaEZ.com. All rights reserved
+ * @license        Creative Commons Attribution-Noncommercial-Share Alike 3.0 Unported
  *
  * Please visit http://joomlaez.com/ for more information
  */
@@ -14,140 +14,116 @@
 // no direct access
 defined('_JEXEC') or die();
 
-class plgSystemJezReCaptchaHelper {
-	// function to check if plugin is enabled and configured
-	function isEnabled() {
-		static $enabled, $params;
+class plgSystemJezReCaptchaHelper
+{
+    // function to check if plugin is enabled and configured
+    public function isEnabled()
+    {
+        static $enabled, $params;
 
-		if (!isset($enabled))
-			$enabled = JPluginHelper::isEnabled('system', 'jezReCaptcha');
+        if (!isset($enabled)) {
+            $enabled = JPluginHelper::isEnabled('system', 'jezReCaptcha');
+        }
 
-		if ($enabled) {
-			// check for necessary params
-			if (!isset($params)) {
-				$plugin	= &JPluginHelper::getPlugin('system', 'jezReCaptcha');
-				$params	= new JParameter($plugin->params);
-			}
+        if ($enabled) {
+            // check for necessary params
+            if (!isset($params)) {
+                $plugin = &JPluginHelper::getPlugin('system', 'jezReCaptcha');
+                $params = new JParameter($plugin->params);
+            }
 
-			if ($params->def('public_key', null) != null && $params->def('private_key', null) != null)
-				return $params;
-		}
+            if ($params->def('public_key', null) != null && $params->def('private_key', null) != null) {
+                return $params;
+            }
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	// function to parse captcha inclusion syntax
-	function parseInclusionSyntax(&$text, $params) {
-		// inclusion syntax matching pattern
-		$regex = '/{\s*captcha\s*}/i';
+    // function to parse captcha inclusion syntax
+    public function parseInclusionSyntax(&$text, $params)
+    {
+        // inclusion syntax matching pattern
+        $regex = '/{\s*captcha\s*}/i';
 
-	 	// find all instances of inclusion syntax and put in $matches
-		preg_match_all($regex, $text, $matches);
-		$count = count($matches[0]);
+        // find all instances of inclusion syntax and put in $matches
+        preg_match_all($regex, $text, $matches);
+        $count = count($matches[0]);
 
-		if ($count) {
-			// replace the first inclusion syntax with real captcha
-			$text = str_replace($matches[0][0], plgSystemJezReCaptchaHelper::renderCaptcha($params), $text);
+        if ($count) {
+            // replace the first inclusion syntax with real captcha
+            $text = str_replace($matches[0][0], plgSystemJezReCaptchaHelper::renderCaptcha($params), $text);
 
-			// captcha should be included only one time per page, clear any remaining inclusion syntax
-			if ($count > 1)
-				plgSystemJezReCaptchaHelper::clearInclusionSyntax($text);
+            // captcha should be included only one time per page, clear any remaining inclusion syntax
+            if ($count > 1) {
+                plgSystemJezReCaptchaHelper::clearInclusionSyntax($text);
+            }
 
-			return true;
-		}
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	// function to clear captcha inclusion syntax
-	function clearInclusionSyntax(&$text) {
-		// inclusion syntax matching pattern
-		$regex = '/{\s*captcha\s*}/i';
+    // function to clear captcha inclusion syntax
+    public function clearInclusionSyntax(&$text)
+    {
+        // inclusion syntax matching pattern
+        $regex = '/{\s*captcha\s*}/i';
 
-		if (preg_match($regex, $text)) {
-			$text = preg_replace($regex, '', $text);
-			return true;
-		}
+        if (preg_match($regex, $text)) {
+            $text = preg_replace($regex, '', $text);
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	// function to render captcha
-	function renderCaptcha($params) {
-		require_once(dirname(__FILE__).DS.'recaptcha-php-1.10/recaptchalib.php');
+    // function to render captcha
+    public function renderCaptcha($params)
+    {
+        //require_once(dirname(__FILE__).DS.'recaptcha-php-1.10/recaptchalib.php');
 
-		$publickey	= trim($params->get('public_key')); // you got this from the signup page
-		
-		$theme = '';
-		if($params->get('theme') == 'custom'){
-		
-			$theme	= "<script> var RecaptchaOptions = {
-						theme	: 'custom',
-						lang	: 'es'
-						};
-						</script>";
-			$captcha = $theme . '
-						<div id="recaptcha_image"></div>
-						<div style="line-height: 2em;"><a href="javascript:Recaptcha.reload()">¿No se puede leer bien?</a></div> 
-						<div style="height: 28px;">
-							<label for="recaptcha_response_field" style="float: left; height: 20px; margin-top: 10px; line-height: 20px;">Ingrese las 2 palabras:</label>
-							<input type="text" id="recaptcha_response_field" name="recaptcha_response_field"  style="margin-top: 10px; float: right;"/>
-						</div>			
-						<script type="text/javascript" src="http://www.google.com/recaptcha/api/challenge?k='.$publickey.'"></script>
-						<noscript>
-							<iframe src="http://www.google.com/recaptcha/api/noscript?k='.$publickey.'"> height="300" width="500" frameborder="0"></iframe>
-							<textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
-							<input type="hidden" name="recaptcha_response_field" value="manual_challenge" />
-						</noscript>
-						';
-		}	
-		else
-			$captcha = recaptcha_get_html($publickey);
-		
-		
+        $publickey = trim($params->get('public_key')); // you got this from the signup page
 
-		return $captcha;
-	}
+        $captcha = '<div class="g-recaptcha" data-sitekey="' . $publickey . '" data-theme="' . $params->get('theme' . 'light') . '"></div>';
+        $captcha .= '<script type="text/javascript" src="https://www.google.com/recaptcha/api.js?hl=es"></script>';
 
-	// function to verify captcha
-	function verifyCaptcha($params) {
-		// get captcha data
-		$challenge = JRequest::getCmd("recaptcha_challenge_field", null, 'POST');
-		$challenge = $challenge == null ? JRequest::getCmd("recaptcha_challenge_field", null, 'GET') : $challenge;
+        return $captcha;
+    }
 
-		// do we have captcha data to verify with reCAPTCHA?
-		if ($challenge != null) {
-			require_once(dirname(__FILE__).DS.'recaptcha-php-1.10/recaptchalib.php');
+    // function to verify captcha
+    public function verifyCaptcha($params)
+    {
+        require_once dirname(dirname(__FILE__)) . DS . 'vendor/autoload.php';
 
-			// get user response
-			$response = JRequest::getCmd("recaptcha_response_field", null, 'POST');
-			$response = $response == null ? JRequest::getCmd("recaptcha_response_field", null, 'GET') : $response;
+        $privatekey = trim($params->get('private_key'));
+        $recaptcha = new \ReCaptcha\ReCaptcha($privatekey);
 
-			$privatekey	= trim($params->get('private_key')); // you got this from the signup page
-			$remoteAddr	= JRequest::getVar('REMOTE_ADDR', '127.0.0.1', 'server');
-			$resp		= recaptcha_check_answer($privatekey, $remoteAddr, $challenge, $response);
+        $gRecaptchaResponse = JRequest::getVar('g-recaptcha-response');
+        $remoteAddr = JRequest::getVar('REMOTE_ADDR', '127.0.0.1', 'server');
 
-			// if verification fail, generate error message then redirect to previous page
-			if (!$resp->is_valid) {
-				JError::raiseWarning('SOME_ERROR_CODE', 'Captcha mal ingresado, vuelva a ingresar las letras de la imagen correctamente. ');
-                      
-				global $mainframe;
-				$httpReferer = JRequest::getVar('HTTP_REFERER', JURI::base(true), 'server');
-				$mainframe->redirect($httpReferer);
-			}
-		}
-	}
+        $resp = $recaptcha->verify($gRecaptchaResponse, $remoteAddr);
 
-	// function to get document type
-	function getDocType() {
-		static $docType;
+        if (!$resp->isSuccess()) {
+            JError::raiseWarning('SOME_ERROR_CODE', 'Captcha mal ingresado, vuelva a ingresar las letras de la imagen correctamente. ');
 
-		if (!isset($docType)) {
-			$document	= &JFactory::getDocument();
-			$docType	= $document->getType();
-		}
+            global $mainframe;
+            $httpReferer = JRequest::getVar('HTTP_REFERER', JURI::base(true), 'server');
+            $mainframe->redirect($httpReferer);
+        }
+    }
 
-		return $docType;
-	}
+    // function to get document type
+    public function getDocType()
+    {
+        static $docType;
+
+        if (!isset($docType)) {
+            $document = &JFactory::getDocument();
+            $docType = $document->getType();
+        }
+
+        return $docType;
+    }
 }
-?>
